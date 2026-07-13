3d28228cdaf0eccff0f338341005aa5fced602bd
Feng GAO <gaofeng21cn@hotmail.com>
2026-07-13 10:46:43 +0800

    fix(settings): enforce typed Flow capability authority

--- packages/desktop/src/renderer/pages/settings/CapabilitiesSettings.tsx
+++ packages/desktop/src/renderer/pages/settings/CapabilitiesSettings.tsx
@@ -1578,8 +1578,8 @@ export const CapabilitiesSettingsContent: React.FC<CapabilitiesSettingsContentPr
     (component) => component.id === 'opl_base'
   )?.dependencyCatalog;
   const flowManagedCatalog = React.useMemo(
-    () => readOplFlowManagedCapabilityCatalog(baseDependencyCatalog, appStateQuery.appState),
-    [appStateQuery.appState, baseDependencyCatalog]
+    () => readOplFlowManagedCapabilityCatalog(baseDependencyCatalog),
+    [baseDependencyCatalog]
   );
 
   const syncFlowCapabilities = async () => {
